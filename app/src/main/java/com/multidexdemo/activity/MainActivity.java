52d7effbc574570bd005a0b4233a729220ec2a64
FlyStorm <ggloading@gmail.com>
2017-09-13 10:20:34 +0800

    项目中有65536以上个方法，分包成功，classes.dex和classes2.dex两个dex文件

--- app/src/main/java/com/multidexdemo/activity/MainActivity.java
+++ app/src/main/java/com/multidexdemo/activity/MainActivity.java
@@ -1,13 +1,29 @@
 package com.multidexdemo.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    //声明控件
+    private Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //实例化控件
+        btn1= (Button) findViewById(R.id.btn1);
+
+        //设置点击监听
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,OtherActivity.class));
+            }
+        });
     }
 }
